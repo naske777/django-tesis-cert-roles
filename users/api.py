76da6bff69b36fea42241d6b1effa4d6bcda59ea
naske <naske777ig@gmail.com>
2024-07-19 15:10:25 -0400

    feat: Add login view

--- users/api.py
+++ users/api.py
@@ -1,10 +1,28 @@
-from rest_framework import viewsets, status
+# api.py
+from django.contrib.auth import authenticate
+from rest_framework_simplejwt.views import TokenObtainPairView
+from rest_framework_simplejwt.tokens import RefreshToken
+from rest_framework.permissions import AllowAny
+from rest_framework import status
 from rest_framework.response import Response
 from .models import *
 from .serializer import *
-
 
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
 
+class LoginView(TokenObtainPairView):
+    permission_classes = (AllowAny,)
+
+    def post(self, request, *args, **kwargs):
+        username = request.data.get('username')
+        password = request.data.get('password')
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            refresh = RefreshToken.for_user(user)
+            return Response({
+                'access': str(refresh.access_token),
+            })
+        else:
+            return Response({'error': 'Credenciales inválidas'}, status=status.HTTP_401_UNAUTHORIZED)
