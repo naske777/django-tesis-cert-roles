758a3c79ba4d1f19c24a93e83eea37c7aa331f7c
naske <naske777ig@gmail.com>
2024-07-19 15:10:05 -0400

    feat: Add userManager

--- users/models.py
+++ users/models.py
@@ -1,13 +1,35 @@
-from django.db import models
-
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
 
 class RoleType(models.TextChoices):
     ADMIN = 'adm', 'Administrador'
     TUTOR = 'tutor', 'Tutor'
     STUDENT = 'student', 'Estudiante'
 
+class UserManager(BaseUserManager):
+    def create_user(self, username, password=None, **extra_fields):
+        if not username:
+            raise ValueError('El nombre de usuario es obligatorio')
+        user = self.model(username=username, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
 
-class User(models.Model):
-    username = models.TextField()
-    password = models.TextField()
+    def create_superuser(self, username, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+
+        return self.create_user(username, password, **extra_fields)
+
+class User(AbstractBaseUser, PermissionsMixin):
+    username = models.TextField(unique=True)
     role = models.CharField(max_length=10, choices=RoleType.choices)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = 'username'
+    REQUIRED_FIELDS = []
+
+    def __str__(self):
+        return self.username
