4f60a938ec76b5d5972976d85c3723c43262942c
naske <naske777ig@gmail.com>
2024-07-21 15:31:14 -0400

    choreL Change fields of user serializer

--- users/serializers.py
+++ users/serializers.py
@@ -12,7 +12,7 @@ class UserSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = User
-        fields = ['id', 'username', 'email', 'profile']
+        fields = ['id', 'username','password', 'profile']
 
     def create(self, validated_data):
         profile_data = validated_data.pop('profile')
