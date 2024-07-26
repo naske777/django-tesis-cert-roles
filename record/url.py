0f6063806a0946eb69e39ab957bd7add68e28a26
naske <naske777ig@gmail.com>
2024-07-25 23:47:53 -0400

    fix: Url position

--- record/url.py
+++ record/url.py
@@ -3,13 +3,13 @@ from rest_framework.routers import DefaultRouter
 from .api import *
 
 router = DefaultRouter()
-router.register(r'', RecordViewSet)
 router.register(r'tasks', TaskViewSet)
 router.register(r'evaluations', EvaluationsViewSet)
 router.register(r'generic_competencies', GenericCompetenciesViewSet)
 router.register(r'specific_competencies', SpecificCompetenciesViewSet)
 router.register(r'diagnoses', DiagnosisViewSet)
 router.register(r'competency', CompetencyViewSet)
+router.register(r'', RecordViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
