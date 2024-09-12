6f18ff234e2ff6338677f18b9f54ca20687604c5
Naske <naske777ig@gmail.com>
2024-09-12 09:32:59 -0400

    revert: Delete jazzmin

--- api/settings.py
+++ api/settings.py
@@ -53,7 +53,6 @@ CSRF_TRUSTED_ORIGINS = [
 # Application definition
 
 INSTALLED_APPS = [
-    'jazzmin',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
