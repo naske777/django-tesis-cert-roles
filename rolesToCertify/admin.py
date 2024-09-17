c38a0c069e12f2fdc7ff0cea824d641773a6c49c
Naske <naske777ig@gmail.com>
2024-09-16 22:46:53 -0400

    feat: Register RoleToCertify model in the admin site

--- rolesToCertify/admin.py
+++ rolesToCertify/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
 from .models import RoleToCertify
 
-# Registra el modelo RoleToCertify en el sitio administrativo
-admin.site.register(RoleToCertify)
+class RoleToCertifyAdmin(admin.ModelAdmin):
+    list_display = ('name')  # Reemplaza con los nombres de los campos de tu modelo
+
+# Registra el modelo RoleToCertify en el sitio administrativo con la clase ModelAdmin
+admin.site.register(RoleToCertify, RoleToCertifyAdmin)
