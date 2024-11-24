6cf976657619b8f33f70ac34fee4ce892a8ca23a
naske777 <naske777ig@gmail.com>
2024-11-24 17:24:10 -0500

    fix: Allow Create user

--- users/api.py
+++ users/api.py
@@ -12,7 +12,6 @@ from rest_framework.permissions import IsAuthenticated
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
-    permission_classes = [IsAuthenticated]
 
 class LoginView(TokenObtainPairView):
     permission_classes = (AllowAny,)
