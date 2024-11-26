ba8c660761031f57dd60ec3ae9a96facc25f0fe9
naske777 <naske777ig@gmail.com>
2024-11-26 11:25:16 -0500

    feat: enhance UserProfile admin with custom form and queryset for tutor_students

--- users/admin.py
+++ users/admin.py
@@ -1,4 +1,19 @@
+from django import forms
 from django.contrib import admin
 from .models import UserProfile
+from students.models import Students
 
-admin.site.register(UserProfile)
+class UserProfileAdminForm(forms.ModelForm):
+    class Meta:
+        model = UserProfile
+        fields = '__all__'
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['tutor_students'].queryset = Students.objects.all()
+        self.fields['tutor_students'].label_from_instance = lambda obj: f"{obj.name}"
+
+class UserProfileAdmin(admin.ModelAdmin):
+    form = UserProfileAdminForm
+
+admin.site.register(UserProfile, UserProfileAdmin)
