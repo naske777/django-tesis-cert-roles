ed15fce440a09271afae84a1b7277118923c06c4
naske777 <naske777ig@gmail.com>
2024-11-26 13:31:57 -0500

    feat: import UserProfile model in StudentViewSet

--- students/api.py
+++ students/api.py
@@ -3,7 +3,7 @@ from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated, AllowAny
 from .models import *
 from .serializer import *
-
+from users.models import UserProfile
 
 class StudentViewSet(viewsets.ModelViewSet):
     queryset = Students.objects.all()
